a2a18a7f9e43d377b2e57eeb5b679276f3c42c1b
emeshDev <emesh.gancan@gmail.com>
2024-12-06 17:00:19 +0700

    make nodejs runtime enabled

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,5 +2,6 @@ import { httpHandler } from "@/server";
 
 // if you want runs on edge, enable this
 // export const runtime = "edge";
+export const runtime = "nodejs";
 
 export { httpHandler as GET, httpHandler as POST };
